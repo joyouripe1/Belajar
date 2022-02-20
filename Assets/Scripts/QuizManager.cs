40573587db442f0c89f92477f8b5c4db88db43b8
joyouripe1 <joyouripe@gmail.com>
2022-02-20 20:42:28 +0700

    latihan hehehe

---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    [Header("HOLDER UIs")]
+
+    public TMP_Text soal;
+
+    public Button[] opsis;
+}
